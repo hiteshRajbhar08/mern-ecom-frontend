f9a6c86035549cd4e51a1c59d1cec654336f4033
Hitesh <h08rajbhar@gmail.com>
2023-01-28 12:35:40 +0530

    footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import Footer from './components/layout/footer/Footer';
 import Header from './components/layout/header/Header';
 
 const App = () => {
   return (
     <Router>
       <Header />
+      <Footer />
     </Router>
   );
 };
